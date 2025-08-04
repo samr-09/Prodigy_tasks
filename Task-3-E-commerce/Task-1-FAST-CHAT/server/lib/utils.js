58ed001959bdb9fbcf46b20c29e55bce150d9fce
samr-09 <samratmondal899@gmail.com>
2025-08-05 01:44:24 +0530

    Remove duplicate Task-1-FAST-CHAT from Task-3-E-commerce

--- Task-3-E-commerce/Task-1-FAST-CHAT/server/lib/utils.js
+++
@@ -1,9 +0,0 @@
-import jwt from "jsonwebtoken";
-
-//function generate token for a user
-
-export const generateToken= (userID)=>{
-const token =jwt.sign({userID}, process.env.JWT_SECRET);
-return token;
-}
-    
